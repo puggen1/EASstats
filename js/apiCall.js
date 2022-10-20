d45bbbcebf834c33a3a8806cc53be5de2351255a
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2022-10-20 10:50:06 +0200

    fixed the local key

--- js/apiCall.js
+++ js/apiCall.js
@@ -1,4 +1,4 @@
-require("dotenv/config");
+//require("dotenv/config");
 /**
  * @description the cors i have to use on localserver and base url for the api
  */
@@ -9,6 +9,7 @@ let baseUrl = "https://api.mozambiquehe.re/";
  * @description personal auth token
  */
 let authToken = __API_KEY__;
+let authTokenLocal = process.env.API_KEY;
 /**
  *
  * @param {string} type what type of information you want to get from the api
@@ -34,7 +35,7 @@ export default async function apiFetch(
   let options = {
     method: "GET",
     headers: {
-      Authorization: authToken,
+      Authorization: authTokenLocal ? authToken : authTokenLocal,
     },
   };
   let result = await fetch(urlTosend, options).then((response) =>
