5c187a2424ebaca07b728f078bc8723b6f490c7d
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2022-10-20 09:49:39 +0200

    removed test files

--- js/apiTest.js
+++
@@ -1,9 +0,0 @@
-require("dotenv/config");
-
-function justForFun(key) {
-  if (key === process.env.API_KEY) {
-    console.log("det stemmer");
-  }
-}
-
-justForFun("ad085523a16414de43978ca12643317e");
